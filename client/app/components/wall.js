adfff572fb972020925a86b6d0483e123e2c83c2
Gabriel Montes <gabriel@utoctadel.com.ar>
2016-12-09 00:47:09 -0300

    Simplifica inicio usando startInterval

--- client/app/components/wall.js
+++ client/app/components/wall.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react'
 import NewPost from './newPost'
 import PostsList from './postsList'
 
+function startInterval(fn, interval) {
+  setTimeout(fn, 0)
+  return setInterval(fn, interval)
+}
+
 export default class Wall extends Component {
   componentDidMount() {
     const { getPosts } = this.props
-    getPosts()
-    setInterval(function () {
+    startInterval(function () {
       getPosts()
     }, 5000)
   }
